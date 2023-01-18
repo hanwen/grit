83f620432bf78b87a7f3f98c34ff532777fd257f
Han-Wen Nienhuys <hanwen@google.com>
2023-01-18 23:06:41 +0100

    server: PatchTree function

---
+++ server/tree.go
@@ -0,0 +1,146 @@
+// Copyright 2023 Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package server
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/filemode"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/plumbing/storer"
+	"github.com/hanwen/glitfs/glitfs"
+)
+
+type lazyTreeNode struct {
+	mode filemode.FileMode
+
+	// for Dir nodes, either id == zero or children != nil
+	id       plumbing.Hash
+	children map[string]*lazyTreeNode
+}
+
+func (n *lazyTreeNode) print(indent int) {
+	fmt.Printf("%*sid=%v {\n", indent, "", n.id)
+	indent++
+	for k, v := range n.children {
+		fmt.Printf("%*s%s:\n", indent, "", k)
+		v.print(indent)
+	}
+	indent--
+	fmt.Printf("%*s}\n", indent, "")
+
+}
+func (n *lazyTreeNode) encode(s storer.EncodedObjectStorer) (plumbing.Hash, error) {
+	if n.id != plumbing.ZeroHash {
+		return n.id, nil
+	}
+
+	if len(n.children) == 0 {
+		return plumbing.ZeroHash, nil
+	}
+
+	var es []object.TreeEntry
+	for k, ch := range n.children {
+		id, err := ch.encode(s)
+		if err != nil {
+			return plumbing.ZeroHash, err
+		}
+
+		if id != plumbing.ZeroHash {
+			es = append(es, object.TreeEntry{
+				Name: k,
+				Hash: id,
+				Mode: ch.mode,
+			})
+		}
+	}
+	glitfs.SortTreeEntries(es)
+
+	t := object.Tree{Entries: es}
+
+	enc := s.NewEncodedObject()
+	enc.SetType(plumbing.TreeObject)
+	if err := t.Encode(enc); err != nil {
+		return plumbing.ZeroHash, err
+	}
+
+	id, err := s.SetEncodedObject(enc)
+	return id, err
+}
+
+func (n *lazyTreeNode) materialize(t *object.Tree) {
+	if n.mode != filemode.Dir || n.children != nil {
+		return
+	}
+
+	n.id = plumbing.ZeroHash
+	n.children = map[string]*lazyTreeNode{}
+	for _, t := range t.Entries {
+		n.children[t.Name] = &lazyTreeNode{
+			id:   t.Hash,
+			mode: t.Mode,
+		}
+	}
+}
+
+func (lt *lazyTreeNode) patch(eos storer.EncodedObjectStorer, entry object.TreeEntry) error {
+	path := entry.Name
+	sepIdx := strings.Index(path, "/")
+	if sepIdx == -1 {
+		if entry.Hash == plumbing.ZeroHash {
+			delete(lt.children, path)
+		} else {
+			lt.children[path] = &lazyTreeNode{
+				id:   entry.Hash,
+				mode: entry.Mode,
+			}
+		}
+	} else {
+		dir := path[:sepIdx]
+		ch := lt.children[dir]
+		if ch == nil || ch.mode != filemode.Dir {
+			ch = &lazyTreeNode{
+				mode:     filemode.Dir,
+				children: map[string]*lazyTreeNode{},
+			}
+			lt.children[dir] = ch
+		} else if ch.id != plumbing.ZeroHash {
+			t, err := object.GetTree(eos, ch.id)
+			if err != nil {
+				return err
+			}
+
+			ch.materialize(t)
+		}
+
+		entry.Name = path[sepIdx+1:]
+		if err := ch.patch(eos, entry); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// PatchTree constructs a new tree by applying changes to it. In changes,
+// the ZeroHash signifies deletion of the path.
+func PatchTree(eos storer.EncodedObjectStorer, t *object.Tree, changes []object.TreeEntry) (id plumbing.Hash, err error) {
+	root := lazyTreeNode{
+		mode: filemode.Dir,
+		id:   t.Hash,
+	}
+	root.materialize(t)
+
+	for _, change := range changes {
+		change.Name = filepath.Clean(change.Name)
+		if err := root.patch(eos, change); err != nil {
+			return id, err
+		}
+	}
+
+	return root.encode(eos)
+}
---
+++ server/tree_test.go
@@ -0,0 +1,110 @@
+// Copyright 2023 Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package server
+
+import (
+	"fmt"
+	"io"
+	"reflect"
+	"testing"
+
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/filemode"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/plumbing/storer"
+	"github.com/go-git/go-git/v5/storage/memory"
+)
+
+func insertBlob(eos storer.EncodedObjectStorer, content string) (id plumbing.Hash, err error) {
+	enc := eos.NewEncodedObject()
+	enc.SetType(plumbing.BlobObject)
+	w, err := enc.Writer()
+	if err != nil {
+		return id, err
+	}
+
+	w.Write([]byte(content))
+	if err := w.Close(); err != nil {
+		return id, err
+	}
+	return eos.SetEncodedObject(enc)
+}
+
+func dumpTree(t *object.Tree) ([]object.TreeEntry, error) {
+	iter := t.Files()
+
+	var entries []object.TreeEntry
+	for {
+		fi, err := iter.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		entries = append(entries,
+			object.TreeEntry{
+				Name: fi.Name,
+				Mode: fi.Mode,
+				Hash: fi.Hash,
+			})
+	}
+	return entries, nil
+}
+
+func checkTree(t *testing.T, eos storer.EncodedObjectStorer, tree *object.Tree, in []object.TreeEntry, want []object.TreeEntry) *object.Tree {
+	id, err := PatchTree(eos, tree, in)
+	if err != nil {
+		t.Fatal(err)
+	}
+	newTree, err := object.GetTree(eos, id)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := dumpTree(newTree)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("checkTree(%v): got %v, want %v", in, got, want)
+	}
+
+	return newTree
+}
+
+func TestPatchTree(t *testing.T) {
+	storer := memory.NewStorage()
+
+	var ids []plumbing.Hash
+	for i := 0; i < 10; i++ {
+		id, err := insertBlob(storer, fmt.Sprintf("%d", i))
+		if err != nil {
+			t.Fatal(err)
+		}
+		ids = append(ids, id)
+	}
+
+	tree := &object.Tree{}
+	tree = checkTree(t, storer, tree, []object.TreeEntry{
+		{Name: "a", Mode: filemode.Regular, Hash: ids[0]},
+		{Name: "b/c", Mode: filemode.Regular, Hash: ids[1]},
+	}, []object.TreeEntry{
+		{Name: "a", Mode: filemode.Regular, Hash: ids[0]},
+		{Name: "b/c", Mode: filemode.Regular, Hash: ids[1]},
+	})
+	tree = checkTree(t, storer, tree, []object.TreeEntry{
+		{Name: "a/q", Mode: filemode.Regular, Hash: ids[3]},            // file -> dir
+		{Name: "b/c", Mode: filemode.Regular, Hash: plumbing.ZeroHash}, // dir becomes empty
+	}, []object.TreeEntry{
+		{Name: "a/q", Mode: filemode.Regular, Hash: ids[3]},
+	})
+	tree = checkTree(t, storer, tree, []object.TreeEntry{
+		{Name: "a", Mode: filemode.Regular, Hash: ids[4]}, // dir -> file
+	}, []object.TreeEntry{
+		{Name: "a", Mode: filemode.Regular, Hash: ids[4]},
+	})
+}
